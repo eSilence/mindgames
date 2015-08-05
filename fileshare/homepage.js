b0cbc7ae9a7c16ee92e6580b4073ad0c8690e4a5
Vladimir Zakharov <zakharov.vv@gmail.com>
2015-08-05 10:55:19 +0300

    fileshare: template and JS

---
+++ fileshare/homepage.js
@@ -0,0 +1,68 @@
+/*global document,alert,FormData,XMLHttpRequest,JSON*/
+function show_progress(filename, progress) {
+    var span = document.getElementById('progress');
+    if(progress === 100) {
+        span.innerHTML = '<a href="' + filename + '">' + filename + '</a>';
+    } else {
+        span.innerHTML = progress + '%';
+    }
+}
+
+
+function check_progress(filename) {
+    'use strict';
+
+    return function() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/?name=' + filename);
+        xhr.send();
+        xhr.onreadystatechange = function() {
+            var data, span;
+            if (xhr.readyState !== 4) {
+                return;
+            }
+            if (xhr.status !== 200) {
+                alert(xhr.status + ': ' + xhr.statusText);
+            } else {
+                data = JSON.parse(xhr.responseText);
+                show_progress(filename, data.progress);
+                if(data.progress !== 100) {
+                    setTimeout(check_progress(filename), 2 * 100);
+                }
+            }
+        };
+    };
+}
+
+
+function post_form() {
+    'use strict';
+
+    var formData = new FormData(document.forms.upload),
+        filename = document.getElementsByName('file')[0].files[0].name,
+        xhr = new XMLHttpRequest();
+    xhr.open('POST', '/', true);
+    xhr.send(formData);
+    xhr.onreadystatechange = function() {
+        var data;
+        if (xhr.readyState !== 4) {
+            return;
+        }
+        if (xhr.status !== 200) {
+            alert(xhr.status + ': ' + xhr.statusText);
+        } else {
+            data = JSON.parse(xhr.responseText);
+            show_progress(filename, data.progress);
+        }
+    };
+    return filename;
+}
+
+
+document.forms.upload.onsubmit = function() {
+    'use strict';
+
+    var filename = post_form();
+    check_progress(filename)();
+    return false;
+};
--- fileshare/server.py
+++ fileshare/server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import asyncio
 import json
+import os
+import os.path
 
 import aiohttp
 import aiohttp.server
@@ -24,7 +26,7 @@ class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
             self.writer, 200, http_version=message.version
         )
         registry = UploadRegistry()
-        if message.method == 'GET':
+        if message.method == 'POST':
             file_name = 'file'
             registry[file_name] = 0
             for i in range(1, 101):
@@ -32,13 +34,41 @@ class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
                 # XXX: sleep only for demonstration purposes
                 yield from asyncio.sleep(0.1)
             data = json.dumps({'progress': 100}).encode()
-            del registry[file_name]
             self.send_json(response, data)
             yield from response.write_eof()
         else:
-            data = json.dumps({'progress': registry.get('file', 100)}).encode()
-            self.send_json(response, data)
+            if '?' in message.path:
+                data = json.dumps({'progress': registry.get('file', '')}).encode()
+                self.send_json(response, data)
+            elif message.path == '/':
+                self.show_form(response)
+            else:
+                file_path = os.path.join(*message.path[1:].split('/'))
+                if os.path.exists(file_path):
+                    chunk_size = 64 * 2 ** 10
+                    with open(file_path, 'rb') as f:
+                        file_size = os.stat(file_path).st_size
+                        response.add_header('Content-type', 'text/html')
+                        response.add_header('Content-Length', str(file_size))
+                        response.send_headers()
+                        while True:
+                            chunk = f.read(chunk_size)
+                            if not chunk:
+                                break
+                            response.write(chunk)
+                else:
+                    self.show_form(response)
             yield from response.write_eof()
+
+    def show_form(self, response):
+        """Вывод начальной страницы с формой загрузки."""
+
+        with open('homepage.html', 'rb') as f:
+            html = f.read()
+            response.add_header('Content-type', 'text/html')
+            response.add_header('Content-Length', str(len(html)))
+            response.send_headers()
+            response.write(html)
 
     def send_json(self, response, data):
         response.add_header('Content-Type', 'text/json')
